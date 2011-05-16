54aa0a89b1abc946957bd656abe99dc2b10284d4
guolicai@gmail.com <guolicai@gmail.com@3e252ee0-2a55-11df-8288-31e8ecb78f5f>
2011-05-16 09:29:01 +0000

    git-svn-id: https://ustcthesis.googlecode.com/svn/trunk@24 3e252ee0-2a55-11df-8288-31e8ecb78f5f

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# this is for Unix/Linux clean scripts
+rm -f  *.aux
+rm -f  chapter/*.aux
+rm -f *.log
+rm -f *.lof
+rm -f *.lot
+rm -f *.bbl
+rm -f *.blg
+rm -f *.thm
+rm -f *.toc
+rm -f *.out
+rm -f *.loa
